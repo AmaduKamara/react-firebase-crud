8af9f98d8f10d7b172cb576a1eade929901c856c
Amkam <amaduskamara@gmail.com>
2022-03-04 15:42:32 +0000

    Add firebase database configuration

---
+++ firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAj8lR9euPbQqO0RROPswiUQvURdfsgfhQ",
+  authDomain: "react-contact-app-e2b5d.firebaseapp.com",
+  projectId: "react-contact-app-e2b5d",
+  storageBucket: "react-contact-app-e2b5d.appspot.com",
+  messagingSenderId: "206580403810",
+  appId: "1:206580403810:web:9cb67ed94d2852d7950b3b",
+};
+
+const fireDb = firebase.initializeApp(firebaseConfig);
+export default fireDb.database().ref();
