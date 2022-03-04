397c3e42cb49e06bf7ce630a3562801a1a5546ef
Amkam <amaduskamara@gmail.com>
2022-03-04 17:25:08 +0000

    Add styles on app bg and home

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const AddEdit = () => {
   return (
-    <div>AddEdit</div>
-  )
-}
+    <div className="container mx-auto flex justify-center items-center my-16">
+      <div className="w-3/4 box-shadow p-12">
+        <h2>Add Contact</h2>
+      </div>
+    </div>
+  );
+};
 
-export default AddEdit
+export default AddEdit;
